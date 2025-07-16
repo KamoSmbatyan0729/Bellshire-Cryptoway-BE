36db26a9dc52f8dbad3c651368abd38920f4ee05
BenjaminOrtiz <benjamin.ortiz0729@gmail.com>
2025-07-16 21:32:48 +0200

    fix issues

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs = require("fs");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const serverRoutes = require("./routes/serverRoutes");
